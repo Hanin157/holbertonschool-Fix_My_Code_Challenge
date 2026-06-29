1f4d3979e7f02523f483e60273909ffbaeb7ba3b
Hanin157 <alshaneen856@gmail.com>
2026-06-29 18:01:53 +0000

    Fix double linked list deletion

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,9 +1,42 @@
-else
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-        tmp = (*head)->next;
-        (*head)->prev->next = tmp;
-        if (tmp != NULL)
-                tmp->prev = (*head)->prev;
-        free(*head);
-        *head = saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	tmp = *head;
+	p = 0;
+
+	while (tmp != NULL && p < index)
+	{
+		tmp = tmp->next;
+		p++;
+	}
+
+	if (tmp == NULL)
+		return (-1);
+
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+	else
+		*head = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
 }
